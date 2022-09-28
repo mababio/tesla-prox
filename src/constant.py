c349cc238a8eabee8044303e133f02440944fd18
Michael Ababio <michael.ababio@contractors.mailchimp.com>
2022-09-28 17:41:59 -0400

    cleaning up added try/execption block and logging and dynaconf

--- src/constant.py
+++ src/constant.py
@@ -1,2 +1,10 @@
 LATHOME, LONHOME = (40.670031, -74.096605)
 HOMERADIUS =  float(0.08359348659472568) #param_prox={'lat':40.669900, 'lon': -74.095629} #float(0.073084957596393)
+
+
+# from config import settings
+#
+# val = settings['production']['HOME_RADIUS']
+# print(val)
+# tt = type(val)
+# print(tt)
