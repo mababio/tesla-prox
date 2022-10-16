f61889444bdbb990d1b598dc799e8ac0ab633d2c
Michael Ababio <michaelkwasi@gmail.com>
2022-10-16 16:58:04 -0400

    bug fix: syntax issue with cloudbuild.yaml 2

--- load_settings.py
+++ load_settings.py
@@ -4,13 +4,13 @@ from google.cloud import secretmanager
 client_secret_manager = secretmanager.SecretManagerServiceClient()
 
 
-def download(project_number):
+def download(project_number, location='settings.toml'):
     # Create GCP managed secret: https://console.cloud.google.com/security/secret-manager
     # Make sure to select upload file option, and upload your local copy of settings.toml
     name_medium_secret = "projects/{}/secrets/tap_settings_files/versions/latest".format(project_number)
     response = client_secret_manager.access_secret_version(name=name_medium_secret)
     pwd = response.payload.data.decode("UTF-8")
-    with open('settings.toml', 'w') as outfile:
+    with open(location, 'w') as outfile:
         outfile.write(pwd)
 
 
@@ -27,9 +27,10 @@ def upload(project_number):
 
 if __name__ == "__main__":
     method = sys.argv[1]
-    method_argument = sys.argv[2]
-    if str(method) == 'download' and method_argument:
-        download(str(method_argument))
-    elif str(method) == 'upload' and method_argument:
-        upload(str(method_argument))
+    project_number = sys.argv[2]
+    download_location = sys.argv[3]
+    if str(method) == 'download' and project_number:
+        download(str(project_number), download_location)
+    elif str(method) == 'upload' and project_number:
+        upload(str(project_number))
 
